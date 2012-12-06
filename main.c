a1ce29b923f9337a9b98e2a76527b44124cc8154
David Lawrence <dlaw@dlaw.me>
2012-12-06 13:45:05 -0500

    working faster buck lookup

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#include "lookup.h"
 #include "main.h"
 
 // PINOUT
@@ -11,8 +12,6 @@
 //  16 (OC1B): buck high side
 //  27 (ADC4): Current sense
 //  28 (ADC5): Vdd divided by 10
-
-#define BUCK_SCALE 15576  // 1298 * buck converter output voltage
 
 int main(void) {
   DDRB = 0xFF;  // output
@@ -23,9 +22,9 @@ int main(void) {
   setup_adc();
   sei();
 
-  set_boost(128);
+  set_boost(50);
   for (;;) {
-    uint16_t buck_setting = BUCK_SCALE / get_vdd();
-    set_buck((buck_setting <= 255) ? buck_setting : 255);
+    set_buck(buck_duty[get_vdd()]);
+    PORTB ^= 1;
   }
 }
