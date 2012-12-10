fddf8782f632389a684455222d70b61f9286ab83
David Lawrence <dlaw@dlaw.me>
2012-12-10 17:49:28 -0500

    Run boost PWM at 120 kHz

--- pwm.c
+++ pwm.c
@@ -14,9 +14,9 @@ inline void setup_pwm(void) {
   TCCR1A |= 1<<WGM10 | 1<<COM1B0 | 1<<COM1B1;
   TCCR1B |= 1<<CS10 | 1<<WGM12;
   TCCR2A |= 1<<WGM20 | 1<<WGM21 | 1<<COM2B0 | 1<<COM2B1;
-  TCCR2B |= 1<<CS20;
+  TCCR2B |= 1<<WGM22 | 1<<CS20;
   TCNT1 = BUCK_DELAY;  // shoot-through delay for the buck
-  TCNT2 = 128;  // run boost out of phase for minor efficiency increase
+  OCR2A = 64;  // run boost at 120 khz
   OCR0B = OCR1B = OCR2B = 255;  // start with buck and boost off
   GTCCR &= ~(1<<TSM);  // unfreeze timers
 }
@@ -40,5 +40,5 @@ ISR(TIMER0_OVF_vect) {
 // Boost output
 inline void set_boost(char duty) {
   if (duty > BOOST_MAX) duty = BOOST_MAX;
-  OCR2B = 255 - duty;
+  OCR2B = 64 - (duty >> 2);
 }
